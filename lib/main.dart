1e5c100d17fa304ad0b8c387b6c0d5f0c2ab8f63
Bob Jackson <yousong.wang@highsoft.ltd>
2022-05-28 16:27:06 +0800

    finish infinite scrolling list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,29 +15,62 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: HelloWorld(),
+      home: const InfiniteScrollingListView(),
     );
   }
 }
 
-class HelloWorld extends StatelessWidget {
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'Welcome to flutter !',
-        theme: ThemeData(
-          primarySwatch: Colors.deepOrange
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to flutter'),
-          ),
-          body: const Center(
-            child: Text('Hello World'),
-          ),
-        ),
-      );
-  }
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider();
+        /*2*/
 
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class InfiniteScrollingListView extends StatelessWidget {
+  const InfiniteScrollingListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: const Center(
+            child: Center(
+          child: RandomWords(),
+        )),
+      ),
+    );
+  }
 }
